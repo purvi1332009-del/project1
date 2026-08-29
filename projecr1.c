6c5a888945933ec74cc643fa7c1dbe2c4883c2ad
Purvi1332009-del <purvi1332009@gmail.com>
2026-08-29 10:25:46 +0530

    Initial commit

---
+++ projecr1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    float p,r,t,simple_interest;
+    printf("enter principal amount:");
+    scanf("%f",&p);
+    printf("enter the rate of interest:");
+    scanf("%f",&r);
+    printf("enter time in years:");
+    scanf("%f",&t);
+    simple_interest=(p*r*t)/100;
+    printf("the simple interest is:%.2f\n",simple_interest);
+    return 0;
+}
