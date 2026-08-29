96fab91f487b7b03272b4741555269de6b7be1fe
Purvi1332009-del <purvi1332009@gmail.com>
2026-08-29 12:05:19 +0530

    project7.c

---
+++ project7.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    float base, height, area;
+    // initializing values
+    printf("enter the base of triangle:");
+    scanf("%f", &base);
+    printf("enter the height of triangle:");
+    scanf("%f", &height);
+    area = (height * base) / 2;
+    printf("the area of the triangle is:%.2f\n", area);
+    return 0;
+}
