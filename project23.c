dc1a5afccf8497a94f435fe0237ccfa5664775fb
Purvi1332009-del <purvi1332009@gmail.com>
2026-09-14 17:20:09 +0530

    project23.c

--- project23.c
+++ project23.c
@@ -1,12 +1,13 @@
-#include <stdio.h>
+#include <stdio.h> //print sum of even num
 int main()
 {
-    int i, sum;
-
-    i = 100;
-
-    sum = i * (i + 1) / 2;
-    printf(" total sum of no. is:%d\n", sum);
-
-    return 0;
+    int i, sum = 0;
+    for (i = 0; i <= 100; i++)
+    {
+        if (i % 2 == 0)
+        {
+            sum = sum + i;
+        }
+        printf("%d\t", sum);
+    }
 }
